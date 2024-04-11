d7b1d19fec2deee0b1b424a34d998fec876b6dba
eunbime <ebk1014@gmail.com>
2024-04-11 23:07:03 +0900

    add: 알고리즘 풀이

---
+++ level1/가장가까운같은글자.js
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/142086
+
+function solution(s) {
+  const hash = {};
+
+  return [...s].map((v, i) => {
+    let result = hash[v] !== undefined ? i - hash[v] : -1;
+    hash[v] = i;
+    return result;
+  });
+}
